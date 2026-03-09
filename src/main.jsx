2a7fa84d135e3530ad50e7da319dc11ff959f154
gerardqueen <gerad.queen583@gmail.com>
2026-03-09 09:34:54 +0000

    Deploy-ready frontend

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import CoachCMS from "./CoachCMS";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <CoachCMS />
+  </React.StrictMode>
+);
